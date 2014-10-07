d6c93610a25267a91341146cb15788acfc09026c
Philippe O. Wagner <wagner@arteria.ch>
2014-10-07 14:46:12 +0200

    Create views.py

---
+++ {{cookiecutter.repo_name}}/{{cookiecutter.app_name}}/views.py
@@ -0,0 +1,8 @@
+#from django.contrib.auth.decorators import login_required
+#from django.shortcuts import render_to_response, get_object_or_404
+#from django.template import RequestContext
+#from django.shortcuts import redirect
+#from django.template.defaultfilters import slugify
+#from django.core.urlresolvers import reverse
+#from django.http import Http404, HttpResponseForbidden, HttpResponseServerError, HttpResponseRedirect  
+

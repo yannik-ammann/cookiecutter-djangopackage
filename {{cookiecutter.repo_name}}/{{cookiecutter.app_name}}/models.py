d37dc2846ee2ac4840f58854273da374cdedf31f
Yannik Ammann <ammann@arteria.ch>
2014-11-05 09:03:01 +0100

    no message

--- {{cookiecutter.repo_name}}/{{cookiecutter.app_name}}/models.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.app_name}}/models.py
@@ -1,14 +1,18 @@
 # -*- coding: utf-8 -*-
-from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
-  
-"""  
+
+"""Models for the {{cookiecutter.app_name}} app."""
+
+# from django.db import models
+# from django.utils.translation import ugettext_lazy as _
+# from django.utils.encoding import python_2_unicode_compatible
+
+"""
 @python_2_unicode_compatible
 class Item(models.Model):
     name = models.CharField(max_length=100)
     items = models.ManyToManyField('self', blank=True)
     private = models.BooleanField(default=True)
-  
+
     def __str__(self):
         return self.name
-"""  
+"""
---
+++ {{cookiecutter.repo_name}}/{{cookiecutter.app_name}}/urls.py
@@ -0,0 +1,13 @@
+"""URLs for the VAR_PACKAGE_NAME app."""
+
+# from django.conf.urls import patterns, url
+
+# from {{cookiecutter.app_name}} import views
+
+
+# urlpatterns = patterns(
+#     '',
+#     url(r'^$',
+#         views.YourView.as_view(),
+#         name='VAR_PACKAGE_NAME_default'),
+# )
